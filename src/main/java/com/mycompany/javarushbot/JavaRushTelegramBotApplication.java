99c33caff10e67f6b398c193105f0795b84e1f03
Ne-Ga-TIV <anton.antonio.10@mail.ru>
2023-07-27 14:42:26 +0300

    Init commit

---
+++ src/main/java/com/mycompany/javarushbot/JavaRushTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.javarushbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRushTelegramBotApplication.class, args);
+	}
+
+}
